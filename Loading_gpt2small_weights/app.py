942e85300f3be37042732f16ef3d1539582461be
Sana-Harshitha <sanaharshitha2@gmail.com>
2025-06-18 18:41:07 +0530

    successfully created all required packages and modules

---
+++ Loading_gpt2small_weights/app.py
@@ -0,0 +1,19 @@
+from LLM_core.GPTModel import GPTModel
+from Backbone_gpt2.model_loader import settings, params
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": True        # Query-Key-Value bias
+}
+
+gpt = GPTModel(GPT_CONFIG_124M )
+gpt.eval()
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
