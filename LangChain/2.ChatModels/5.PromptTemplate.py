a53a58130fd58e4e8e6aeeb3079e2058d45c5c82
Sana-Harshitha <sanaharshitha2@gmail.com>
2025-07-21 15:19:10 +0530

    Ignored environment variables

--- LangChain/2.ChatModels/5.PromptTemplate.py
+++ LangChain/2.ChatModels/5.PromptTemplate.py
@@ -1,8 +1,15 @@
-
+import os
+from dotenv import load_dotenv  
 from langchain_cohere import ChatCohere
 from langchain.prompts import PromptTemplate
+
+# Load environment variables
+load_dotenv()   
+# Get the Cohere API key from environment variables
+cohere_api_key = os.getenv("COHERE_API_KEY")
+
 # Initialize Cohere LLM
-llm = ChatCohere(cohere_api_key="Wg5jgYLIffSDxJHnIeCkXNOI15GDQ2PlR4iKhAlg", model="command-r-plus")
+llm = ChatCohere(cohere_api_key=cohere_api_key, model="command-r-plus")
 
 # Create the template
 prompt = PromptTemplate(
